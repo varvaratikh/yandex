73c2d869e1ecdce78a821aa1aa484427bef64a66
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-09-20 19:27:31 +0300

    create button's components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import { Main } from "./layouts/main/Main";
+import { Button } from "./components/button/Button";
 
 export const App = () => {
-  return <Main />;
+  return (
+    <>
+      <Button
+        backgroundColor="#FFD11C"
+        borderColor="#FFD11C"
+        textColor="#141414"
+        margin="10px"
+        padding="15px 30px"
+      >
+        Зарегистрироваться
+      </Button>
+
+      <Button
+        backgroundColor="transparent"
+        borderColor="#F7F9FB"
+        textColor="#F7F9FB"
+        margin="10px"
+        padding="20px 52px"
+      >
+        Войти
+      </Button>
+    </>
+  );
 };
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface ButtonProps {
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  margin?: string;
+  padding?: string;
+  children: React.ReactNode;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  backgroundColor,
+  borderColor = "transparent",
+  textColor,
+  margin = "0",
+  padding = "10px 20px",
+  children,
+}) => {
+  return (
+    <button
+      style={{
+        backgroundColor: backgroundColor,
+        margin: margin,
+        padding: padding,
+        color: textColor,
+        border: `1px solid ${borderColor}`,
+        borderRadius: "20px",
+        cursor: "pointer",
+        height: "63px",
+        fontSize: "18px",
+        fontWeight: "500",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
